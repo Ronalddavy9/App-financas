f25c37cc66753456d4dee0fa29406887419a2e9d
Ronald davy <ronalddavy9@gmail.com>
2023-04-18 14:19:26 -0300

    first commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 150px;
+  text-align: center;
+  background: #8498BF;
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #0D0D0D;
+`;
